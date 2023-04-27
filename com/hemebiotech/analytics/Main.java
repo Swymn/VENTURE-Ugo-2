eff8e6886fd6dbfd253007441aed903028b5d799
Ugo Venture <80dn5d2slx32-student@anonymous-codio.com>
2023-04-27 10:03:42 +0000

    main implementation

--- com/hemebiotech/analytics/Main.java
+++ com/hemebiotech/analytics/Main.java
@@ -3,4 +3,18 @@ package com.hemebiotech.analytics;
 import java.util.List;
 import java.util.Map;
 
-// WRITE YOUR CODE HERE
+public class Main {
+
+    public static void main(String args[]) {
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile();
+
+        AnalyticsCounter analytics = new AnalyticsCounter(reader, writer);
+
+        List<String> symptoms = analytics.getSymptoms();
+        Map<String, Integer> count = analytics.countSymptoms(symptoms);
+        Map<String, Integer> sort = analytics.sortSymptoms(count);
+
+        analytics.writeSymptoms(sort);
+    }
+}
